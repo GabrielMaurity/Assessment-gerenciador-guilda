b2fd8fc86689791d24a9581933653aedace4899d
Gabriel <gfmaurity@gmail.com>
2026-04-21 16:29:24 -0300

    unecessary comments

--- src/main/java/br/com/infnet/guilda_dos_aventureiros/Repository/ProdutoLojaRepository.java
+++ src/main/java/br/com/infnet/guilda_dos_aventureiros/Repository/ProdutoLojaRepository.java
@@ -8,18 +8,13 @@ import java.util.List;
 public interface ProdutoLojaRepository
         extends ElasticsearchRepository<ProdutoLoja, String> {
 
-    // Busca por nome (text search)
     List<ProdutoLoja> findByNome(String nome);
 
-    // Filtro exato por categoria (keyword)
     List<ProdutoLoja> findByCategoria(String categoria);
 
-    // Filtro exato por raridade (keyword)
     List<ProdutoLoja> findByRaridade(String raridade);
 
-    // Filtro por preço máximo
     List<ProdutoLoja> findByPrecoBetween(Double min, Double max);
 
-    // Combinação de filtros
     List<ProdutoLoja> findByCategoriaAndRaridade(String categoria, String raridade);
 }
